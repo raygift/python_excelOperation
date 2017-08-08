d362586c8e0a02d271f16cb5875b11c7ae1cabe7
raygift <the_bigticket@outlook.com>
2017-08-08 23:08:26 +0800

    分不同类实现具体功能

---
+++ dataOperation.py
@@ -0,0 +1,8 @@
+import pyExcelOperation
+
+class dataOperation:
+    def readOriginData(self,filepath,date):
+        filename="origin"+date+".xlsx"
+
+    def writeTargetData(self,filepath,date):
+        filename="target"+date+".xlsx"
--- pyExcelOperation.py
+++ pyExcelOperation.py
@@ -54,24 +54,5 @@ class easyExcel:
             sht.Cells(bottomRow, rightCol)
             ).Value = data
 
-
-if __name__=="__main__":
-    targetFile=easyExcel(r'C:/Users/bigticket/Desktop/python_excel/targetFile.xlsx')
-    excelFile=easyExcel(r'C:/Users/bigticket/Desktop/python_excel/origFile.xlsx')
-    multilist=excelFile.getRangeValue("广告统计信息汇总",4,4,217,7)
-    spmlist=targetFile.getRangeValue("PC",6,3,35,3)
-    targetlist= [[0 for col in range(1)] for row in range(30)]
-
-    for i in range(len(spmlist)):
-        for j in range(len(multilist)):
-            if spmlist[i][0]==multilist[j][0]:
-                targetlist[i][0]=multilist[j][3] 
-
-    rangeObj = targetFile.getRange("PC",1,36,5,36)
-    rangeObj.EntireColumn.Insert()
-    targetFile.setRange("PC",6,35,targetlist)
-
-    excelFile.close()
-
-    targetFile.save()
-    targetFile.close()
+    # def findColPosition():
+    #     "在targetExcel中找到最后一天的记录，并在其右侧插入空白列"
